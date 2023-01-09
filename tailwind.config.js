c4b3f6b9a6000520fc54de5996d1003a2ead652e
Sarah <sarahkvburnett@gmail.com>
2023-01-09 00:02:16 +0000

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    content: ['dist/**/*.html'],
+    theme: {
+        colors: {
+            'orange': '#FF9900',
+            'purple': '#9100FF',
+            'blue': '#08fdd8',
+            'pink': '#ff2253',
+            'black': '#121212',
+            'white': '#fff'
+        },
+        fontFamily: {
+            sans: ['Poppins', 'sans-serif'],
+        },
+        fontSize: {
+            'sm': '0.8rem',
+            'base': '1rem',
+            'xl': '1.8rem',
+            '2xl': '2.5rem',
+            '3xl': '3.3rem',
+            '4xl': '5rem'
+        }
+    },
+    plugins: [],
+}
